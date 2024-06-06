bee2e0ee94b975e55fd5128371bbba1276e06795
DebyAyuSyakhira <ddebys18@gmail.com>
2024-06-06 22:02:52 +0800

    judul apk

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ void main() {
   runApp(
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "Application",
+      title: "Application latihannnnnnn",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
     ),
